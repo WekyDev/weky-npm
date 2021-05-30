ddf843a74fa3223bcd698d19cc1e21dd53fd2cb1
birb <62964708+birbified@users.noreply.github.com>
2021-05-30 10:57:04 -0400

    sudo

--- data/class/sudo.js
+++ data/class/sudo.js
@@ -8,23 +8,24 @@ class sudo {
      * @param {any} [options.member] member
      */
     constructor(options) {
-        if(!options.text) throw new TypeError('Missing argument: text')
-        if(typeof options.text !== 'string') throw new TypeError('Text must be in a string')
-        if(!options.member) throw new TypeError('Missing argument: member')
-        if(!options.message) throw new TypeError('Missing argument: message')
+        if(!options.text) throw new TypeError('Missing argument: text');
+        if(typeof options.text !== 'string') throw new TypeError('Text must be in a string');
+        if(!options.member) throw new TypeError('Missing argument: member');
+        if(!options.message) throw new TypeError('Missing argument: message');
     
         this.message = options.message;
-        this.text = options.text
-        this.member = options.member
+        this.text = options.text;
+        this.member = options.member;
     }
+    
     async start() {
         this.message.channel.createWebhook(this.member.user.username, {
             avatar: this.member.user.displayAvatarURL({ dynamic: true })
         }).then(webhook => {
-            webhook.send(this.text)
+            webhook.send(this.text);
             setTimeout(() => {
-                webhook.delete()
-            }, 3000)
+                webhook.delete();
+            }, 3000);
         })
 
     }
