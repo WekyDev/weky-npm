6ff6c86fc74c03b60a380179327fa60fb2f726b9
Rox0z <leonxlok1@hotmail.com>
2021-06-05 14:16:25 -0300

    optmize calc

--- data/functions/calculator.js
+++ data/functions/calculator.js
@@ -1,238 +1,133 @@
 module.exports = async (message) => {
-    if (!message) throw new TypeError("Weky Error: Missing argument message")
-    const { MessageButton } = require('discord-buttons')
-    function i(length) {
-     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-     var result = '';
-     for ( var i = 0; i < length; i++ ) {
-         result += randomChars.charAt(Math.floor(Math.random() * randomChars.length))
-     }
-     return result
-   }
-    let str = ' '
-    let stringify = '```\n' + str + '\n```'
-    let calculator_ac = i(20)
-    let calculator_e1 = i(20)
-    let calculator_e2 = i(20)
-    let calculator_uppercase = i(20)
-    let calculator_7 = i(20)
-    let calculator_8 = i(20)
-    let calculator_9 = i(20)
-    let calculator_plus = i(20)
-    let calculator_minus = i(20)
-    let calculator_star = i(20)
-    let calculator_devide = i(20)
-    let calculator_1 = i(20)
-    let calculator_2 = i(20)
-    let calculator_3 = i(20)
-    let calculator_4 = i(20)
-    let calculator_5 = i(20)
-    let calculator_0 = i(20)
-    let calculator_6 = i(20)
-    let calculator_dot = i(20)
-    let calculator_equal = i(20)
-    let ac = new MessageButton().setLabel('AC').setID(calculator_ac).setStyle('red')
-    let e1 = new MessageButton().setLabel('(').setID(calculator_e1).setStyle('blurple')
-    let e2 = new MessageButton().setLabel(')').setID(calculator_e2).setStyle('blurple')
-    let uppercase = new MessageButton().setLabel('^').setID(calculator_uppercase).setStyle('blurple')
-    let seven = new MessageButton().setLabel('7️').setID(calculator_7).setStyle('gray')
-    let eight = new MessageButton().setLabel('8️').setID(calculator_8).setStyle('gray')
-    let nine = new MessageButton().setLabel('9️').setID(calculator_9).setStyle('gray')
-    let slash = new MessageButton().setLabel('/').setID(calculator_devide).setStyle('blurple')
-    let four = new MessageButton().setLabel('4️').setID(calculator_4).setStyle('gray')
-    let five = new MessageButton().setLabel('5️').setID(calculator_5).setStyle('gray')
-    let six = new MessageButton().setLabel('6️').setID(calculator_6).setStyle('gray')
-    let star = new MessageButton().setLabel('*').setID(calculator_star).setStyle('blurple')
-    let one = new MessageButton().setLabel('1️').setID(calculator_1).setStyle('gray')
-    let two = new MessageButton().setLabel('2️').setID(calculator_2).setStyle('gray')
-    let three = new MessageButton().setLabel('3️').setID(calculator_3).setStyle('gray')
-    let minus = new MessageButton().setLabel('-').setID(calculator_minus).setStyle('blurple')
-    let zero = new MessageButton().setLabel('0️').setID(calculator_0).setStyle('gray')
-    let dot = new MessageButton().setLabel('.').setID(calculator_dot).setStyle('blurple')
-    let equal = new MessageButton().setLabel('=').setID(calculator_equal).setStyle('green')
-    let plus = new MessageButton().setLabel('+').setID(calculator_plus).setStyle('blurple')
-    //Lock
-    let qac = new MessageButton().setLabel('AC').setID(calculator_ac).setStyle('red').setDisabled()
-    let qe1 = new MessageButton().setLabel('(').setID(calculator_e1).setStyle('blurple').setDisabled()
-    let qe2 = new MessageButton().setLabel(')').setID(calculator_e2).setStyle('blurple').setDisabled()
-    let quppercase = new MessageButton().setLabel('^').setID(calculator_uppercase).setStyle('blurple').setDisabled()
-    let qseven = new MessageButton().setLabel('7️').setID(calculator_7).setStyle('gray').setDisabled()
-    let qeight = new MessageButton().setLabel('8️').setID(calculator_8).setStyle('gray').setDisabled()
-    let qnine = new MessageButton().setLabel('9️').setID(calculator_9).setStyle('gray').setDisabled()
-    let qslash = new MessageButton().setLabel('/').setID(calculator_devide).setStyle('blurple').setDisabled()
-    let qfour = new MessageButton().setLabel('4️').setID(calculator_4).setStyle('gray').setDisabled()
-    let qfive = new MessageButton().setLabel('5️').setID(calculator_5).setStyle('gray').setDisabled()
-    let qsix = new MessageButton().setLabel('6️').setID(calculator_6).setStyle('gray').setDisabled()
-    let qstar = new MessageButton().setLabel('*').setID(calculator_star).setStyle('blurple').setDisabled()
-    let qone = new MessageButton().setLabel('1️').setID(calculator_1).setStyle('gray').setDisabled()
-    let qtwo = new MessageButton().setLabel('2️').setID(calculator_2).setStyle('gray').setDisabled()
-    let qthree = new MessageButton().setLabel('3️').setID(calculator_3).setStyle('gray').setDisabled()
-    let qminus = new MessageButton().setLabel('-').setID(calculator_minus).setStyle('blurple').setDisabled()
-    let qzero = new MessageButton().setLabel('0️').setID(calculator_0).setStyle('gray').setDisabled()
-    let qdot = new MessageButton().setLabel('.').setID(calculator_dot).setStyle('blurple').setDisabled()
-    let qequal = new MessageButton().setLabel('=').setID(calculator_equal).setStyle('green').setDisabled()
-    let qplus = new MessageButton().setLabel('+').setID(calculator_plus).setStyle('blurple').setDisabled()
-    //-----------------------
-    const filter = m => m.clicker.user.id == message.author.id;
-    message.channel.send(stringify, {
-      components: [
-     {type: 1, components: [
-   ac, e1, e2, uppercase
-     ]},
-     {type: 1, components: [
-   seven, eight, nine, slash
-     ]},
-     {type: 1, components: [
-   four, five, six, star
-     ]},
-       {type: 1, components: [
-   one, two, three, minus
-     ]},
-       {type: 1, components: [
-   dot, zero, equal, plus
-     ]},
-   ]
-   }
-   ).then( async (msg) => {
-   async function edit(){
-     msg.edit(stringify, {
-       components: [
-      {type: 1, components: [
-    ac, e1, e2, uppercase
-      ]},
-      {type: 1, components: [
-    seven, eight, nine, slash
-      ]},
-      {type: 1, components: [
-    four, five, six, star
-      ]},
-        {type: 1, components: [
-    one, two, three, minus
-      ]},
-        {type: 1, components: [
-         dot, zero, equal, plus
-   ]},
-    ]
-    }
-    )
-   }
-   async function lock(){
-     msg.edit(stringify, {
-       components: [
-      {type: 1, components: [
-    qac, qe1, qe2, quppercase
-      ]},
-      {type: 1, components: [
-    qseven, qeight, qnine, qslash
-      ]},
-      {type: 1, components: [
-    qfour, qfive, qsix, qstar
-      ]},
-        {type: 1, components: [
-    qone, qtwo, qthree, qminus
-      ]},
-        {type: 1, components: [
-         qdot, qzero, qequal, qplus
-      ]},
-    ]
-    }
-    )
-   }
-   const calc = msg.createButtonCollector(filter)
-   
-   calc.on('collect', async btn => {
-     btn.defer()
-     if(btn.id === calculator_1){
-       str += '1'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_2){
-       str += '2'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_3){
-       str += '3'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_4){
-       str += '4'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_5){
-       str += '5'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_6){
-       str += '6'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_7){
-       str += '7'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_8){
-       str += '8'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_9){
-       str += '9'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_0){
-       str += '0'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_plus){
-       str += '+'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_minus){
-       str += '-'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_devide){
-       str += '/'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_uppercase){
-       str += '^'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_star){
-       str += '*'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_dot){
-       str += '.'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_ac){
-       str = ' '
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_e1){
-       str += '('
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_e2){
-       str += ')'
-       stringify = '```\n' + str + '\n```'
-       edit()
-     }else if(btn.id === calculator_equal){
-       try{
-       str += ' = ' + require('mathjs').evaluate(str)+''
-       stringify = '```\n' + str + '\n```'
-       edit()
-       str = ''
-       stringify = '```\n' + str + '\n```'
-       }catch(e){
-
-         message.channel.send(' An invalid equation was entered!').then((m) => m.delete({ timeout: 2000 }))
-
-         stringify = '```\n' + str + '\n```'
-       }
-     }
-   })
-   
-   })
+  if (!message) throw new TypeError("Missing argument message");
+  const { MessageButton: btn } = require("discord-buttons");
+  function s(len) {
+    for (var rChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", res = "", i = 0; i < len; i++) res += rChar.charAt(Math.floor(Math.random() * rChar.length));
+    return res;
   }
+  let str = " ",
+    strf = "```\n" + str + "\n```",
+    ac = s(20),
+    e1 = s(20),
+    e2 = s(20),
+    up = s(20),
+    b7 = s(20),
+    b8 = s(20),
+    b9 = s(20),
+    add = s(20),
+    sub = s(20),
+    mul = s(20),
+    div = s(20),
+    b1 = s(20),
+    b2 = s(20),
+    b3 = s(20),
+    b4 = s(20),
+    b5 = s(20),
+    b0 = s(20),
+    b6 = s(20),
+    dot = s(20),
+    eql = s(20),
+    BAC = new btn().setLabel("AC").setID(ac).setStyle("red"),
+    BE1 = new btn().setLabel("(").setID(e1).setStyle("blurple"),
+    BE2 = new btn().setLabel(")").setID(e2).setStyle("blurple"),
+    BUP = new btn().setLabel("^").setID(up).setStyle("blurple"),
+    BT7 = new btn().setLabel("7️").setID(b7).setStyle("gray"),
+    BT8 = new btn().setLabel("8️").setID(b8).setStyle("gray"),
+    BT9 = new btn().setLabel("9️").setID(b9).setStyle("gray"),
+    DIV = new btn().setLabel("÷").setID(div).setStyle("blurple"),
+    BT4 = new btn().setLabel("4️").setID(b4).setStyle("gray"),
+    BT5 = new btn().setLabel("5️").setID(b5).setStyle("gray"),
+    BT6 = new btn().setLabel("6️").setID(b6).setStyle("gray"),
+    MUL = new btn().setLabel("×").setID(mul).setStyle("blurple"),
+    BT1 = new btn().setLabel("1️").setID(b1).setStyle("gray"),
+    BT2 = new btn().setLabel("2️").setID(b2).setStyle("gray"),
+    BT3 = new btn().setLabel("3️").setID(b3).setStyle("gray"),
+    SUB = new btn().setLabel("-").setID(sub).setStyle("blurple"),
+    BT0 = new btn().setLabel("0️").setID(b0).setStyle("gray"),
+    DOT = new btn().setLabel(".").setID(dot).setStyle("blurple"),
+    EQL = new btn().setLabel("=").setID(eql).setStyle("green"),
+    ADD = new btn().setLabel("+").setID(add).setStyle("blurple"),
+    LAC = new btn().setLabel("AC").setID(ac).setStyle("red").setDisabled(),
+    LE1 = new btn().setLabel("(").setID(e1).setStyle("blurple").setDisabled(),
+    LE2 = new btn().setLabel(")").setID(e2).setStyle("blurple").setDisabled(),
+    LUP = new btn().setLabel("^").setID(up).setStyle("blurple").setDisabled(),
+    LB7 = new btn().setLabel("7️").setID(b7).setStyle("gray").setDisabled(),
+    LB8 = new btn().setLabel("8️").setID(b8).setStyle("gray").setDisabled(),
+    LB9 = new btn().setLabel("9️").setID(b9).setStyle("gray").setDisabled(),
+    LDV = new btn().setLabel("÷").setID(div).setStyle("blurple").setDisabled(),
+    LB4 = new btn().setLabel("4️").setID(b4).setStyle("gray").setDisabled(),
+    LB5 = new btn().setLabel("5️").setID(b5).setStyle("gray").setDisabled(),
+    LB6 = new btn().setLabel("6️").setID(b6).setStyle("gray").setDisabled(),
+    LML = new btn().setLabel("×").setID(mul).setStyle("blurple").setDisabled(),
+    LB1 = new btn().setLabel("1️").setID(b1).setStyle("gray").setDisabled(),
+    LB2 = new btn().setLabel("2️").setID(b2).setStyle("gray").setDisabled(),
+    LB3 = new btn().setLabel("3️").setID(b3).setStyle("gray").setDisabled(),
+    LSB = new btn().setLabel("-").setID(sub).setStyle("blurple").setDisabled(),
+    LB0 = new btn().setLabel("0️").setID(b0).setStyle("gray").setDisabled(),
+    LDT = new btn().setLabel(".").setID(dot).setStyle("blurple").setDisabled(),
+    LEQ = new btn().setLabel("=").setID(eql).setStyle("green").setDisabled(),
+    LAD = new btn().setLabel("+").setID(add).setStyle("blurple").setDisabled();
+  const filter = (t) => t.clicker.user.id == message.author.id;
+  message.channel
+    .send(strf, {
+      components: [
+        { type: 1, components: [BAC, BE1, BE2, BUP] },
+        { type: 1, components: [BT7, BT8, BT9, DIV] },
+        { type: 1, components: [BT4, BT5, BT6, MUL] },
+        { type: 1, components: [BT1, BT2, BT3, SUB] },
+        { type: 1, components: [DOT, BT0, EQL, ADD] },
+      ],
+    })
+    .then(async (msg) => {
+      async function edit() {
+        msg.edit(strf, {
+          components: [
+            { type: 1, components: [BAC, BE1, BE2, BUP] },
+            { type: 1, components: [BT7, BT8, BT9, DIV] },
+            { type: 1, components: [BT4, BT5, BT6, MUL] },
+            { type: 1, components: [BT1, BT2, BT3, SUB] },
+            { type: 1, components: [DOT, BT0, EQL, ADD] },
+          ],
+        });
+      }
+      async function lock() {
+        msg.edit(strf, {
+          components: [
+            { type: 1, components: [LAC, LE1, LE2, LUP] },
+            { type: 1, components: [LB7, LB8, LB9, LDV] },
+            { type: 1, components: [LB4, LB5, LB6, LML] },
+            { type: 1, components: [LB1, LB2, LB3, LSB] },
+            { type: 1, components: [LDT, LB0, LEQ, LAD] },
+          ],
+        });
+      }
+      msg.createButtonCollector(filter).on("collect", async (btn) => {
+        btn.defer()
+        if (btn.id === b1) (strf = "```\n" + (str += "1") + "\n```"), edit();
+        else if (btn.id === b2) (strf = "```\n" + (str += "2") + "\n```"), edit();
+        else if (btn.id === b3) (strf = "```\n" + (str += "3") + "\n```"), edit();
+        else if (btn.id === b4) (strf = "```\n" + (str += "4") + "\n```"), edit();
+        else if (btn.id === b5) (strf = "```\n" + (str += "5") + "\n```"), edit();
+        else if (btn.id === b6) (strf = "```\n" + (str += "6") + "\n```"), edit();
+        else if (btn.id === b7) (strf = "```\n" + (str += "7") + "\n```"), edit();
+        else if (btn.id === b8) (strf = "```\n" + (str += "8") + "\n```"), edit();
+        else if (btn.id === b9) (strf = "```\n" + (str += "9") + "\n```"), edit();
+        else if (btn.id === b0) (strf = "```\n" + (str += "0") + "\n```"), edit();
+        else if (btn.id === add) (strf = "```\n" + (str += "+") + "\n```"), edit();
+        else if (btn.id === sub) (strf = "```\n" + (str += "-") + "\n```"), edit();
+        else if (btn.id === div) (strf = "```\n" + (str += "÷") + "\n```"), edit();
+        else if (btn.id === up) (strf = "```\n" + (str += "^") + "\n```"), edit();
+        else if (btn.id === mul) (strf = "```\n" + (str += "×") + "\n```"), edit();
+        else if (btn.id === dot) (strf = "```\n" + (str += ".") + "\n```"), edit();
+        else if (btn.id === ac) (strf = "```\n" + (str = " ") + "\n```"), edit();
+        else if (btn.id === e1) (strf = "```\n" + (str += "(") + "\n```"), edit();
+        else if (btn.id === e2) (strf = "```\n" + (str += ")") + "\n```"), edit();
+        else if (btn.id === eql)
+          try {
+            (str += " = " + require("mathjs").evaluate(str.replace(/×/g, '*').replace(/÷/g, '/'))), (strf = "```\n" + str + "\n```"), edit(), (strf = "```\n" + (str = "") + "\n```");
+          } catch (t) {
+            message.channel.send(" An invalid equation was entered!").then((e) => e.delete({ timeout: 2e3 })), (strf = "```\n" + str + "\n```");
+          }
+      });
+    });
+};
